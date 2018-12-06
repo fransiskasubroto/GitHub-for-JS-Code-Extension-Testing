da78df3e71b1641f28cede35ad4b19d975aabce1
fransiskasubroto <fransiska.subroto@tealium.com>
2018-12-05 16:28:51 -0800

    German.js
    German

---
+++ German.js
@@ -0,0 +1,51 @@
+var test2;
+const mod = "modified";
+console.log("Frau Meier geht heute in den Supermarkt. Ihr Mann ist nicht zu Hause und sie nimmt den Bus. An der Haltestelle trifft sie ihre Freundin Frau Schmidt. Frau Schmidt will auch in den Supermarkt.\n" +
+    "\n" +
+    "Frau Müller: “Das ist super! Dann fahren wir zusammen!”.\n" +
+    "Frau Schmidt: “Ja, wir kaufen ein. Danach trinken wir Kaffee und essen Kuchen. Im Café neben dem Supermarkt gibt es sehr guten Kuchen.”\n" +
+    "Frau Meier: “Gute Idee!”.\n" +
+    "\n" +
+    "Frau Meier und Frau Schmidt gehen zusammen in den Supermarkt.\n" +
+    "\n" +
+    "Frau Meier: “Ich brauche Tomaten. Mein Mann will Salat essen. Ich nehme zehn Tomaten.”\n" +
+    "Frau Schmidt: “Tomaten sind gut. Ich kaufe fünf Tomaten. Da ist auch Kopfsalat.”\n" +
+    "\n" +
+    "Frau Meier nimmt keinen Kopfsalat. Sie nimmt aber zwei Gurken. Frau Meier kauft auch ein Kilo Zwiebeln. Frau Schmidt will Brot kaufen.\n" +
+    "\n" +
+    "Frau Meier: “Das Brot ist nicht schön. Ich kaufe Brot in der Bäckerei. Aber die Schokolade hier ist gut. Sie kostet nur 50 Cent. Ich nehme drei Tafeln Schokolade.”\n" +
+    "Frau Schmidt: “Da ist noch ein Sonderangebot. Mineralwasser und Orangensaft sind billig.”\n" +
+    "Frau Meier: “Orangensaft habe ich zu Hause. Aber ich brauche fünf Flaschen Mineralwasser.”\n" +
+    "\n" +
+    "Frau Schmidt kauft nur drei Flaschen Mineralwasser. Frau Meier und Frau Schmidt gehen an die Kasse. Dann gehen sie ins Café. Frau Schmidt trinkt eine Tasse Tee. Frau Meier mag lieber Kaffee. Sie bestellen zwei Stücke Schokoladenkuchen. Sie fahren mit dem Bus nach Hause.\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var hey ="LOOK AT ME!";
