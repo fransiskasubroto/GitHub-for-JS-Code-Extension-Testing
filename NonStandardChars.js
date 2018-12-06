85f1e36d60333891a5c46236ad2889ed7dd22ecd
fransiskasubroto <fransiska.subroto@tealium.com>
2018-12-06 09:56:08 -0800

    Non Standard Chars
    Non Standard Chars

---
+++ NonStandardChars.js
@@ -0,0 +1,38 @@
+var test1;
+let a = '¡£™ •¡ª™£ºª•¢¶•¡ª¶£™¢§§¶™•§£¢';
+console.log(a);
+const mod = 'modified';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const m = "mult commits";
+console.warn("TADA!");
+
+
+
+
+
+
+
+
+
+
+
+
+var hey ="LOOK AT ME!";
